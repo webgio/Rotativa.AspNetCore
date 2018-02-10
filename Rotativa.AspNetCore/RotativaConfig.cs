cfaffbdb4ec3835950e92c2aeb69654ef33139b1
webgio <giorgio.bozio@gmail.com>
2018-02-10 18:24:20 +0100

    folder check

--- Rotativa.AspNetCore/RotativaConfig.cs
+++ Rotativa.AspNetCore/RotativaConfig.cs
@@ -22,9 +22,19 @@ namespace Rotativa.AspNetCore
                 return _RotativaPath;
             }
         }
+        /// <summary>
+        /// Setup Rotativa library
+        /// </summary>
+        /// <param name="env">The IHostingEnvironment object</param>
+        /// <param name="wkhtmltopdfRelativePath">Optional. Relative path to the directory containing wkhtmltopdf.exe. Default is "Rotativa". Download at https://wkhtmltopdf.org/downloads.html</param>
         public static void Setup(IHostingEnvironment env, string wkhtmltopdfRelativePath = "Rotativa") 
         {
             var rotativaPath = Path.Combine(env.WebRootPath, wkhtmltopdfRelativePath);
+
+            if (!Directory.Exists(rotativaPath))
+            {
+                throw new ApplicationException("Folder containing wkhtmltopdf.exe not found, searched for " + rotativaPath);
+            }
 
             _RotativaPath = rotativaPath;
         }
