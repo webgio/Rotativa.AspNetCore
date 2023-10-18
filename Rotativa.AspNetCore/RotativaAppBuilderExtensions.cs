b055f05ff18f8b55a75af11edb0918943780444f
webgio <giorgio.bozio@gmail.com>
2023-10-18 19:01:46 +0200

    .net 7

--- Rotativa.AspNetCore/RotativaAppBuilderExtensions.cs
+++ Rotativa.AspNetCore/RotativaAppBuilderExtensions.cs
@@ -56,18 +56,18 @@ namespace Rotativa.AspNetCore
 #endif
 
 #if NET6_0_OR_GREATER
-    public static class RotativaAppBuilderExtensions
+    public static class MvcServiceCollectionExtensions
     {
-        public static IApplicationBuilder UseRotativa(this IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env) 
+        public static IApplicationBuilder UseRotativa(this IApplicationBuilder app)
         {
-            var webApp = app as ApplicationBuilder;
+            var webApp = app as WebApplication;
 
-            if (webApp == null) 
+            if (webApp == null)
             {
                 throw new Exception("Sorry, you can use Rotativa only in a WebApplication");
             }
 
-            RotativaConfiguration.Setup(env.WebRootPath);
+            RotativaConfiguration.Setup(webApp.Environment.WebRootPath);
 
             return app;
 
